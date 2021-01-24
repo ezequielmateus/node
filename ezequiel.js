9c9e58c6b99d3f7350875323da95785e3538c063
unknown <rock.ownar@hotmail.com>
2021-01-24 12:47:15 -0300

    Instalação do EJS

--- ezequiel.js
+++ ezequiel.js
@@ -1,10 +1,26 @@
-var http = require("http");
+var express = require("express")();
+express.set("view engine", "ejs");
 
-var server = http.createServer(function(requisicao, resposta){
+express.get("/", function(req, res){
+    res.send("<h1>Página principal</h1>");
+})
 
-    var req = requisicao.url;
-    resposta.end("<h1>Valor na tela</h1>");
+express.get("/tecnologia", function(req, res){
+    res.send("<h1>Página Tecnologia</h1>");
+})
+express.listen(3000, function(req, res){
+
 
 });
 
-server.listen(3000);
+
+// var http = require("http");
+
+// var server = http.createServer(function(requisicao, resposta){
+
+//     var req = requisicao.url;
+//     resposta.end("<h1>Valor na tela</h1>");
+
+// });
+
+// server.listen(3000);
